9987abc53eb9f283a693f828abbdf5fc1f28d878
IbrahimAlmobea <38829392+IbrahimAlmobea@users.noreply.github.com>
2018-04-29 09:59:18 +0300

    Update Welcome.java
    edited file

--- Welcome.java
+++ Welcome.java
@@ -1,12 +1,12 @@
 package lab01;
 
-public class Welcome2 {
+public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
             System.out.println("welcome to java #2 ");
-         int y = 8;
-        int x = 2;
+         int y = 9;
+        int x = 6;
         System.out.println("x + y =" +(x + y));
         
 	}
