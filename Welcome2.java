0a0e50189405d299299343396e97381f12e0dfb2
IbrahimAlmobea <38829392+IbrahimAlmobea@users.noreply.github.com>
2018-04-29 09:34:08 +0300

    Add files via upload

---
+++ Welcome2.java
@@ -0,0 +1,14 @@
+package lab01;
+
+public class Welcome2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("welcome to java");
+         int y = 5;
+        int x = 9;
+        System.out.println("x + y =" +(x + y));
+        
+	}
+
+}
