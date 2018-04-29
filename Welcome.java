66c9b85793a8091182ea4cfb2f1ec573767d9b3d
IbrahimAlmobea <38829392+IbrahimAlmobea@users.noreply.github.com>
2018-04-29 09:54:44 +0300

    Add file
    added initial file

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+package lab01;
+
+public class Welcome2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("welcome to java #2 ");
+         int y = 8;
+        int x = 2;
+        System.out.println("x + y =" +(x + y));
+        
+	}
+
+}
